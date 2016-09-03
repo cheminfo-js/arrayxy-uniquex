ac69090f97b92754b8552e0df680720984952d29
lpatiny <gb252243>
2016-09-03 14:57:00 +0200

    Update docs

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 
 /**
  * In place modification of the 2 arrays to make X unique and sum the Y if X has the same value
- * @param xs
- * @param ys
+ * @param xs : Array containing an array of the X elements
+ * @param ys : Array containing an array of the Y elements
  */
 
 function uniqueX(xs, ys) {
     if (xs.length < 2) return;
-
+    if (xs.length !== ys.length) {
+        throw new Error('The X and Y arrays mush have the same length');
+    }
+    
     var current = xs[0];
     var counter = 0;
 
